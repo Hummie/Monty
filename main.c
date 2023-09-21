8ff59980d68e34a3f7f165a6c71fa5c8951529ba
Hummie <humphreywill77@gmail.com>
2023-09-21 14:39:16 -0700

    Main function

---
+++ main.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @ac: args counter
+ * @argv: array of args
+ * Return: 0
+*/
+
+char **opcodes_arr = NULL;
+
+int main(int ac, char **argv)
+{
+	size_t line_n = 1;
+	FILE *fd;
+	char *cmd_buffer = NULL;
+	stack_t *stack = NULL;
+	size_t buffer_size = 0;
+	ssize_t readline;
+
+	if (ac != 2)
+	{
+		fprintf(stderr, "Usage: monty file\n");
+		free(cmd_buffer);
+		exit(EXIT_FAILURE);
+	}
+
+	fd = fopen(argv[1], "r");
+	if (!fd)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((readline = getline(&cmd_buffer, &buffer_size, fd)) != -1)
+	{
+		opcodes_arr = tokenize_line(cmd_buffer);
+		execute(&stack, opcodes_arr, line_n);
+		line_n++;
+	}
+	free(cmd_buffer);
+	free(opcodes_arr);
+	fclose(fd);
+	return (0);
+}
+
